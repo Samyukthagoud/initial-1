dcae6c16e4107053b51b7bc71b08dcd3e9f7145b
anitasurna1108@gmail.com <anitasurna1108@gmail.com>
2018-11-28 15:10:31 +0530

    added 3

--- demo.java
+++ demo.java
@@ -3,5 +3,10 @@ class demo1
 public static void main(String []args)
 {
 system.out.println("soft drinks");
+system.out.println("cold drinks");
+system.out.println("soft drinks");
+system.out.println("soft drinks");
+
+
 }
 }
